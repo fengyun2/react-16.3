05e60ff67c97d325708d8be37047ec6cd923ad8c
ly2011 <1518550424@qq.com>
2018-02-11 22:28:43 +0800

    添加React Context API demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// 创建context实例
+const ThemeContext = React.createContext(
+  {
+    // background: 'red',
+    // color: 'white'
+  }
+);
+
+// Title组件
+class Title extends Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {context => (
+          <h1
+            style={{
+              background: context.background,
+              color: context.color,
+              fontSize: context.fontSize,
+              padding: context.padding
+            }}
+          >
+            {this.props.children}
+          </h1>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+// 定义Header组件
+class Header extends Component {
+  render() {
+    return <Title>Hello React Context API.</Title>;
+  }
+}
+// 定义App组件，用到了Provider组件,类似于react-redux的Provider组件
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ThemeContext.Provider
+          value={{
+            background: 'green',
+            color: 'white',
+            fontSize: '32px',
+            padding: '15px'
+          }}
+        >
+          <Header />
+        </ThemeContext.Provider>
       </div>
     );
   }
